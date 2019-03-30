4dac2622284793435e064796404b73e8d3d5dc8e
Sarah Flynn <sarah.flynn@icloud.com>
2019-03-30 16:51:19 -0700

    changed id to match instructions

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
-window.onload = function () {
-  var input = document.getElementById("name").focus();
-}
+window.onload = function() {
+  var input = document.getElementById('name').focus();
+};
 
 function checkOption(value) {
-  const otherTextInput = document.getElementById('other-text');
-  
+  const otherTextInput = document.getElementById('other-title');
+
   if (value === 'other') {
     otherTextInput.style.display = 'block';
   }
