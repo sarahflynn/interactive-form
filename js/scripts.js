3cee404aef451810bfa2b2ff5f793da7fa2b3ce2
Sarah Flynn <sarah.flynn@icloud.com>
2019-03-30 16:50:28 -0700

    other text box conditionally displaying for job title

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
 window.onload = function () {
   var input = document.getElementById("name").focus();
 }
+
+function checkOption(value) {
+  const otherTextInput = document.getElementById('other-text');
+  
+  if (value === 'other') {
+    otherTextInput.style.display = 'block';
+  }
+}
